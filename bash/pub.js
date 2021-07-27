85cb1652b82e9698192fd8767ffdf889294bcbab
zsh <zsh@qq.com>
2021-07-27 22:01:10 +0800

    添加图片

--- bash/pub.js
+++ bash/pub.js
@@ -20,15 +20,22 @@ fix.forEach(fix => {
 })
 
 function replace(file) {
-    const newCode = $.loadFile(file, {
-        parseOptions: {
-            language: 'html'
-        }
-    }).replace('<ol></ol>', `<ol>
-        ${els.join('')}
-    </ol>`).generate()
-
-    fs.writeFileSync(file, newCode, console.log);
+    fs.writeFileSync(file, `
+        <!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8"/>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>images</title>
+        </head>
+        <body>
+            <ol>
+                ${els.join('')}
+            </ol>
+        </body>
+        </html>
+    `, console.log);
 }
 
 replace(path.resolve(__dirname, '../index.html'))
