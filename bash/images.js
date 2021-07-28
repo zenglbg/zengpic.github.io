b627e89c58f4d2a2c9d448b309d8c9f6e163e359
zsh <zsh@qq.com>
2021-07-28 10:04:30 +0800

    添加图片

---
+++ bash/images.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const glob = require('glob')
+const fs = require('fs')
+const $ = require('gogocode');
+const html = require('./html')
+const rootDist = path.resolve(__dirname, '../images')
+
+
+data = {}
+function read(p) {
+    const { length } = arrs = p.split('/');
+    fs.readdirSync(p).forEach(d => {
+        if (fs.lstatSync(path.join(p, d)).isFile()) {
+            if (arrs[length - 1] === 'images') {
+                if (Array.isArray(data['index'])) {
+                    data['index'].push(`https://pic.zenglbg.com/images/${d}`)
+                } else {
+                    data['index'] = [`https://pic.zenglbg.com/images/${d}`]
+                }
+            } else {
+                console.log(p);
+                if (Array.isArray(data[arrs[length - 1]])) {
+                    data[arrs[length - 1]].push(`https://pic.zenglbg.com/${arrs.slice(arrs.indexOf('images'), length).join('/')
+                        }/${d}`)
+                } else {
+                    data[arrs[length - 1]] = [`https://pic.zenglbg.com/${arrs.slice(arrs.indexOf('images'), length).join('/')
+                        }/${d}`]
+                }
+            }
+        } else {
+            read(path.join(p, d))
+        }
+    })
+}
+read(rootDist)
+
+function getLi(images) {
+    Object.keys(images).forEach((image, index, arr) => {
+        html.replace(path.resolve(__dirname, `../${image}.html`), html.getHtml(
+            {
+                children: `
+                    <ul>
+                        ${arr.map(a => `<li><a href="/${a}.html" alt="${a}">${a}</a></li>`).join('')}
+                    </ul>
+                ` + images[image].map(i => {
+                    return `<li style="width: 100%">
+                        <p>${i}</p>
+                        <img style="width: 100%" src="${i}" alt="${i}" />
+                    </li>`
+                }).join(''),
+                title: image
+            }
+        ))
+    })
+}
+
+getLi(data)
+
--- bash/pub.js
+++ bash/pub.js
@@ -1,48 +1,9 @@
 const shelljs = require('shelljs')
-const path = require('path')
-const glob = require('glob')
-const fs = require('fs')
-const $ = require('gogocode');
+require('./images')
 
-els = []
-const fix = ["PNG", "JPG", "png", 'jpg', 'jpeg']
-fix.forEach(fix => {
-
-    glob.sync(path.resolve(__dirname, `../images/**/*.${fix}`)).forEach(file => {
-        const filename = file.split('images')
-        els.push(`
-         <li style="width: 100%">
-            <p>https://pic.zenglbg.com/images${filename[filename.length - 1]}</p>
-            <img style="width: 100%" src="https://pic.zenglbg.com/images${filename[filename.length - 1]}" alt="https://pic.zenglbg.com/images${filename[filename.length - 1]}"/>
-         </li>
-        `)
-    })
-
-})
-
-function replace(file) {
-    fs.writeFileSync(file, `
-        <!doctype html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8"/>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <title>images</title>
-        </head>
-        <body>
-            <ol style="width: 100%">
-                ${els.join('')}
-            </ol>
-        </body>
-        </html>
-    `, console.log);
+function pub() {
+    shelljs.exec(`git add . && git commit -m '添加图片' && git push`)
 }
 
-replace(path.resolve(__dirname, '../index.html'))
 
-
-shelljs.exec(`git add . && git commit -m '添加图片' && git push`)
-
-
-
+pub()
