1ab6654ec310c2f06da5a5b9469fc8f0c14e22a6
zsh <zsh@qq.com>
2022-02-04 16:04:02 +0800

    添加图片

---
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from '/Users/lbg/code/self/zengpic.github.io/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
