06757bce2770964f95f3c1060b5200496f5048f4
zsh <zsh@qq.com>
2023-03-04 13:28:55 +0700

    添加github ci/cd 工作流

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,15 @@
-// import { defineUserConfig } from "vuepress";
+import { defineUserConfig } from "vuepress";
 
-// export default defineUserConfig({
-//   lang: "zh-CN",
-//   title: "你好， zlbg ！",
-//   description: "zlbg的静态资源站",
-//   plugins: [],
-// });
-
-module.exports = {
+export default defineUserConfig({
   lang: "zh-CN",
   title: "你好， zlbg ！",
   description: "zlbg的静态资源站",
   plugins: [],
-};
+});
+
+// module.exports = {
+//   lang: "zh-CN",
+//   title: "你好， zlbg ！",
+//   description: "zlbg的静态资源站",
+//   plugins: [],
+// };
