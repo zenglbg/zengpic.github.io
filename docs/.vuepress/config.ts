bc55e4806506d0bc3fddc1d22fc39e1ae3f8365d
zsh <zsh@qq.com>
2023-03-04 13:28:40 +0700

    添加github ci工作流

--- docs/.vuepress/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineUserConfig } from "vuepress";
-import { docsearchPlugin } from "@vuepress/plugin-docsearch";
-
-export default defineUserConfig({
-  lang: "zh-CN",
-  title: "你好， zlbg ！",
-  description: "zlbg的静态资源站",
-  plugins: [],
-});
