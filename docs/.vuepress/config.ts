bf6311f8ff5ca0a64941dafd204acfb8583c846a
zsh <zsh@qq.com>
2023-03-09 09:19:11 +0700

    更改;配置

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,16 @@
+import { defineUserConfig } from "vuepress";
+
+export default defineUserConfig({
+  base: "/blog/",
+  lang: "zh-CN",
+  title: "你好， zlbg ！",
+  description: "zlbg的静态资源站",
+  plugins: [],
+});
+
+// module.exports = {
+//   lang: "zh-CN",
+//   title: "你好， zlbg ！",
+//   description: "zlbg的静态资源站",
+//   plugins: [],
+// };
